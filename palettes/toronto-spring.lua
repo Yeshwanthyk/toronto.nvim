84075b5a5c0ac8d50904cd3e217fb281f52a3f80
Yesh Yendamuri <kyendamuri@getresq.com>
2025-08-13 20:21:24 -0400

    initial commit

---
+++ palettes/toronto-spring.lua
@@ -0,0 +1,27 @@
+local palette = {
+  bg            = "#1e1e2e",  -- Base - main background
+  fg            = "#cdd6f4",  -- Text - main foreground
+  dim           = "#6c7086",  -- Overlay0 - dimmed text
+  comment       = "#7f849c",  -- Overlay1 - comments
+  string        = "#b4befe",  -- Lavender - soft purple for strings
+  number        = "#89dceb",  -- Sky - spring sky blue for numbers
+  keyword       = "#cdd6f4",  -- Text - main text for keywords
+  method        = "#94e2d5",  -- Teal - spring teal for methods
+  class         = "#a6e3a1",  -- Green - consistent spring green for classes
+  type_builtin  = "#89dceb",  -- Sky - spring sky for built-in types
+  variable      = "#bac2de",  -- Subtext1 - variables
+  decorator     = "#fab387",  -- Peach - warm accent for decorators
+  red           = "#f38ba8",  -- Red
+  yellow        = "#f9e2af",  -- Yellow - spring yellow
+  cyan          = "#94e2d5",  -- Teal - spring cyan
+  
+  -- UI tints using Catppuccin surface colors
+  ui_bg         = "#313244",  -- Surface0
+  ui_bg_alt     = "#45475a",  -- Surface1
+  cursorline    = "#313244",  -- Surface0 for cursor line
+  visual        = "#45475a",  -- Surface1 for selection
+  border        = "#585b70",  -- Surface2 for borders
+}
+
+return palette
+
